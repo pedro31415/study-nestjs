b4073ec2fc6db1272341339547b54085ee3d94f0
pedro31415 <pedrogoku80@gmail.com>
2023-12-23 11:04:53 -0300

    add PartialType

--- src/courses/dto/update-course-dto.ts
+++ src/courses/dto/update-course-dto.ts
@@ -1,5 +1,4 @@
-export class UpdateCourseDTO {
-    name: string
-    description: string
-    tags: string[]
-}
+import { PartialType } from "@nestjs/mapped-types"
+import { CreateCourseDTO } from "./create-course-dto";
+
+export class UpdateCourseDTO extends PartialType(CreateCourseDTO) {}
