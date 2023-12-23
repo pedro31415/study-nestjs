98e2c22a660b0e5f67149d8d50faac9c537fa772
pedro31415 <pedrogoku80@gmail.com>
2023-12-23 11:03:39 -0300

    add class-validator

--- src/courses/dto/create-course-dto.ts
+++ src/courses/dto/create-course-dto.ts
@@ -1,5 +1,12 @@
+import { IsString } from "class-validator"
+
 export class CreateCourseDTO {
-    name: string
-    description: string
-    tags: string[]
+    @IsString()
+    readonly name: string;
+
+    @IsString()
+    readonly description: string;
+
+    @IsString({each: true})
+    readonly tags: string[];
 }
